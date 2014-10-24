6457e6b00ed209dfdd4f6ecacc31b92276ec7e89
Kristofer Carta <kriscarta@gmail.com>
2014-10-24 11:43:13 +0200

    Add Exercise_22_01

---
+++ Exercise_22_01/src/PhoneCall.java
@@ -0,0 +1,37 @@
+
+public class PhoneCall extends BillableItem
+{
+   // Declare instance variables
+   private int minutes;
+   private double pricePerMinute;
+   
+   // Constructor
+   public PhoneCall(String date, String recipient, int minutes, double pricePerMinute)
+   {
+      // Call the super's constructor
+      super(date, recipient);
+      
+      // Then deal with the instance variables
+      this.minutes = minutes;
+      this.pricePerMinute = pricePerMinute;
+   }
+   
+   // Getters
+   public int getMinutes()
+   {
+      return this.minutes;
+   }
+   
+   public double getPricePerMinute()
+   {
+      return this.pricePerMinute;
+   }
+   
+   // Implement the getPrice outlined in the UML diagram
+   @Override
+   public double getPrice()
+   {
+      return this.minutes * this.pricePerMinute;
+   }
+
+}
