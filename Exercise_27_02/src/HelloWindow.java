d54c040e0f1844e91e15d7a98cf790469a170f7e
Kristofer Carta <kriscarta@gmail.com>
2014-11-06 11:47:52 +0100

    Fix Exercise_27_02

--- Exercise_27_02/src/HelloWindow.java
+++ Exercise_27_02/src/HelloWindow.java
@@ -13,15 +13,9 @@ public class HelloWindow extends JFrame
    private static final long serialVersionUID = 1L;
    
    // Declare the window elements
-<<<<<<< HEAD
    private JButton button = new JButton("Click me!");
    private JTextField text = new JTextField("World");
    private JFrame frame;
-=======
-   JButton button = new JButton("Click me!");
-   JTextField text = new JTextField("World");
-   JFrame frame;
->>>>>>> a08f4fade57b5b196abbe760925a2176b79acd16
 
    public HelloWindow()
    {
