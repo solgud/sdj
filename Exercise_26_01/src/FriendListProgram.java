2a14d2ad1f02ab4c0b9afd65a8dec23207e1b020
Kristofer Carta <kriscarta@gmail.com>
2014-11-08 12:37:13 +0100

    Add Exercise_26_01

--- Exercise_26_01/src/FriendListProgram.java
+++ Exercise_26_01/src/FriendListProgram.java
@@ -1,6 +1,7 @@
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 
 public class FriendListProgram
@@ -12,25 +13,40 @@ public class FriendListProgram
       // Write the names to a specified file
       // First input is filename
       // Program ends when input is DONE
-      String fileName;
-      
+      Scanner keyboard = new Scanner(System.in);
+      System.out.println("Enter filename: ");
+      String fileName = keyboard.nextLine();
+
+      // Top try block
+      // To check for file exceptions
       try {
          // Create the PrintWriter
          FileOutputStream outputStream = new FileOutputStream(fileName);
          PrintWriter out = new PrintWriter(outputStream);
-         // Print to PrintWriter
-         out.println();
+         // Second try block
+         // To check for file writing exceptions
+         try {
+            // Do one iteration before loop begins
+            // To allow for zero-name entries
+            System.out.print("Friend name: ");
+            String friendName = keyboard.nextLine();
+            // Then start the loop
+            while(!friendName.equals("DONE"))
+            {
+               // Start by printing, so that DONE is never written to the file
+               out.println(friendName);
+               System.out.print("Friend name: ");
+               friendName = keyboard.nextLine();
+            }
+         }
+         // Allways make sure the PrintWriter closes
+         finally {
+            out.close();
+         }
       } catch (FileNotFoundException e) {
          System.out.println("No such file");
-      } finally {
-         // Close the PrintWriter
-         out.close();
       }
-
-      
-      
-      
-
+      // Close the Scanner
+      keyboard.close();
    }
-
 }
