4005a836b338cc53d09770c72deb7adcda4b517b
Kristofer Carta <kriscarta@gmail.com>
2014-11-08 12:37:29 +0100

    Update Exercise_27_03

--- Exercise_27_03/src/ConverterWindow.java
+++ Exercise_27_03/src/ConverterWindow.java
@@ -1,13 +1,6 @@
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class ConverterWindow extends JFrame
 {
