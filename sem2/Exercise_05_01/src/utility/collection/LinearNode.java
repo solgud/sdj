27480eff6bd707c02370590b66daa02788f352b1
Kristofer Carta <kriscarta@gmail.com>
2015-02-20 09:43:17 +0100

    Update Exercise_05_01

---
+++ sem2/Exercise_05_01/src/utility/collection/LinearNode.java
@@ -0,0 +1,37 @@
+package utility.collection;
+
+public class LinearNode<T> {
+	private T element;
+	private LinearNode<T> next;
+
+	public LinearNode() {
+
+	}
+	
+	public LinearNode(T element) {
+		
+	}
+	
+	public LinearNode(T element, LinearNode<T> next) {
+		
+	}
+	
+	public LinearNode<T> getNext() {
+		
+		return null;
+	}
+	
+	public void setNext(LinearNode<T> node) {
+		
+	}
+	
+	public T getElement() {
+		return null;
+	}
+	
+	public void setElement(T element) {
+		
+	}
+
+
+}
---
+++ sem2/Exercise_05_01/src/utility/collection/LinkedList.java
@@ -0,0 +1,83 @@
+package utility.collection;
+
+public class LinkedList<T> implements ListADT<T> {
+	private int count;
+	private LinearNode<T> front;
+	
+	public LinkedList(){
+		
+	}
+	
+	public String toString() {
+		
+		return null;
+	}
+	
+	public LinearNode<T> getNode(int index) {
+
+		return null;
+	}
+
+	@Override
+	public void add(int index, T element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void add(T element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void set(int index, T element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T remove(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T remove(T element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int indexOf(T element) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean contains(T element) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+
+}
