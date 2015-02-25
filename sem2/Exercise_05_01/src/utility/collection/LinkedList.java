50c47c5401890940de78d1b347b2fbf37a466394
Kristofer Carta <kriscarta@gmail.com>
2015-02-25 10:53:23 +0100

    Add Exercise_06_01 and update Exercise_05_01

--- sem2/Exercise_05_01/src/utility/collection/LinkedList.java
+++ sem2/Exercise_05_01/src/utility/collection/LinkedList.java
@@ -1,25 +1,31 @@
 package utility.collection;
 
-public class LinkedList<T> implements ListADT<T> {
+public class LinkedList<T> implements ListADT<T>
+{
 	private int count;
 	private LinearNode<T> front;
 
-	public LinkedList() {
+	public LinkedList()
+	{
 		front = new LinearNode<T>(); // Dummy node
 		count = 0;
 	}
 
-	public LinearNode<T> getNode(int index) {
+	public LinearNode<T> getNode(int index)
+	{
 		LinearNode<T> current = front; // Start at front node
-		for (int i = -1; i < index; i++) {
+		for (int i = -1; i < index; i++)
+		{
 			current = current.getNext(); // cycle through nodes
 		}
 		return current;
 	}
 
 	@Override
-	public void add(int index, T element) {
-		if (index < 0 || index > count) {
+	public void add(int index, T element)
+	{
+		if (index < 0 || index > count)
+		{
 			throw new IndexOutOfBoundsException();
 		}
 
@@ -30,41 +36,66 @@ public class LinkedList<T> implements ListADT<T> {
 	}
 
 	@Override
-	public void add(T element) {
+	public void add(T element)
+	{
 		LinearNode<T> current = front;
-		while (current.getNext() != null) {
+		while (current.getNext() != null)
+		{
 			current = current.getNext();
 		}
 		current.setNext(new LinearNode<T>(element));
 		count++;
+		
+		/*
+		 * Can be one-lined:
+		 * add(count, element);
+		 */
 	}
 
 	@Override
-	public void set(int index, T element) {
+	public void set(int index, T element)
+	{
+		if(index < 0)
+		{
+			throw new IndexOutOfBoundsException();
+		}
 		getNode(index).setElement(element);
 	}
 
 	@Override
-	public T get(int index) {
+	public T get(int index)
+	{
+		if(index < 0)
+		{
+			throw new IndexOutOfBoundsException();
+		}
 		return getNode(index).getElement();
 	}
 
 	@Override
-	public T remove(int index) {
+	public T remove(int index)
+	{
+		if(index < 0 || index >= count) // Account for bad arguments
+		{
+			throw new IndexOutOfBoundsException();
+		}
 		LinearNode<T> previous = getNode(index - 1); // Get the node right before the one we want to delete
 		LinearNode<T> nodeToDelete = previous.getNext(); // Get the node we want to delete
 		previous.setNext(nodeToDelete.getNext()); // Set the previous node to point to the node after the one we want to delete
 		nodeToDelete.setNext(null); // Remove the deleted node's pointer
 
+		count--;
 		return nodeToDelete.getElement(); // Return the element at the deleted node
 	}
 
 	@Override
-	public T remove(T element) {
+	public T remove(T element)
+	{
 		T elementToRemove = null;
 		int index = indexOf(element);
 
-		if (index > -1) {
+		if (index > -1)
+		{
 			elementToRemove = remove(index);
 		}
 
@@ -72,17 +103,22 @@ public class LinkedList<T> implements ListADT<T> {
 	}
 
 	@Override
-	public int indexOf(T element) {
+	public int indexOf(T element)
+	{
 		int index = 0; // Start index at 0
 		LinearNode<T> current = front; // Start with the first node
-		while (current.getNext() != null) {
+		while (current.getNext() != null)
+		{
 			current = current.getNext(); // Move to the next node
-			if (element == null) { // If looking for a null element
-				if (current.getElement() == null) { // If the current node has a null element
+			if (element == null)
+			{ // If looking for a null element
+				if (current.getElement() == null)
+				{ // If the current node has a null element
 					return index; // Then return the current index
 				}
 			} else if (current.getElement() != null
-					&& current.getElement().equals(element)) { // If the node has a matching element
+					&& current.getElement().equals(element))
+			{ // If the node has a matching element
 				return index; // Return the current index
 			}
 			index++;
@@ -91,29 +127,36 @@ public class LinkedList<T> implements ListADT<T> {
 	}
 
 	@Override
-	public boolean contains(T element) {
+	public boolean contains(T element)
+	{
 		return indexOf(element) != -1;
 	}
 
 	@Override
-	public boolean isEmpty() {
+	public boolean isEmpty()
+	{
 		return count == 0;
 	}
 
 	@Override
-	public int size() {
+	public int size()
+	{
 		return count;
 	}
 
 	@Override
-	public String toString() {
+	public String toString()
+	{
 		String str = "";
 		LinearNode<T> current = front; // Start with the first node
-		while (current.getNext() != null) {
+		while (current.getNext() != null)
+		{
 			current = current.getNext(); // Move off of the dummy node
-			if (current.getElement() == null) { // If current node has no element
+			if (current.getElement() == null)
+			{ // If current node has no element
 				str += "Empty"; // Report as empty
-			} else {
+			} else
+			{
 				str += current.getElement().toString(); // Otherwise, add element's toString to the string
 			}
 			str += "\n";
